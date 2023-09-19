52157bfc66a25dba088ba1433a0a8ca4b527445c
ashwinvinayan13 <ashwinvinayan1306@gmail.com>
2023-09-20 00:21:39 +0530

    second lay for button

---
+++ app/src/main/java/com/looney/premium_scr_layout/PremiumNextActivity.kt
@@ -0,0 +1,11 @@
+package com.looney.premium_scr_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PremiumNextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.premium_next)
+    }
+}
